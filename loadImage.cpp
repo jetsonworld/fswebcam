cd7dba02e0939e550b0d29e528848e877280c3da
Jetson User Group For Korean <59607996+jetsonworld@users.noreply.github.com>
2020-03-04 02:54:04 +0900

    Update loadImage.cpp

--- loadImage.cpp
+++ loadImage.cpp
@@ -6,10 +6,9 @@ using namespace cv;
 
 
 int main() {
-    Mat image = imread("images/lena.jpg", IMREAD_COLOR);
+    Mat image = imread("images/image.jpg", IMREAD_COLOR);
     imshow("Output", image);
     waitKey(0); 
 
     return 0;
 }
-
